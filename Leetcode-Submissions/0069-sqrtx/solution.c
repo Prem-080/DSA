1ca14c25a8c529776e1e106c19716d0798166f9e
Prem Kumar Singh <premkumarsingh355@gmail.com>
2024-02-13 18:39:44 +0000

    0069 - Sqrt(x) - Runtime - 84 ms (5.37%), Memory - 5.4 MB (100.00%)

---
+++ Leetcode-Submissions/0069-sqrtx/solution.c
@@ -0,0 +1,16 @@
+  int mySqrt(int x) {
+
+        int ans, i;
+
+        
+
+        for(i = 1; i <= sqrt(x);i++){
+
+           ans = (i) * (i);
+            
+        }    
+    
+             return (i-1);
+         
+
+    }
