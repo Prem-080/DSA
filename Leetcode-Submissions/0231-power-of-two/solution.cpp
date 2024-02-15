29e92f4e37c924b41e8e983f06dc1cef62e3eeeb
Prem Kumar Singh <premkumarsingh355@gmail.com>
2024-02-15 14:23:43 +0000

    0231 - Power of Two - Runtime - 2 ms (5.14%), Memory - 7 MB (100.00%)

---
+++ Leetcode-Submissions/0231-power-of-two/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int count = 0;
+        if(n > 0){
+        while(n != 0){
+            if(n&1){
+                count++;
+            }
+            n >>=1;
+        }
+            }
+        if(count == 1){ 
+            return 1;
+         }
+        return 0;
+            
+    }
+};
