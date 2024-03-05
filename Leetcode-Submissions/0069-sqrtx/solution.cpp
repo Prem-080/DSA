9ff84231b972288ce735084ac78e81c9de817182
Prem Kumar Singh <premkumarsingh355@gmail.com>
2024-03-05 09:46:28 +0000

    0069 - Sqrt(x) - Runtime - 0 ms (100.00%), Memory - 7.4 MB (100.00%)

--- Leetcode-Submissions/0069-sqrtx/solution.cpp
+++ Leetcode-Submissions/0069-sqrtx/solution.cpp
@@ -1,33 +1,31 @@
+int sqrt(int x){
+    int s = 0;
+    int e = x;
+    long long int mid = s + (e - s)/2;
+    
+    int ans = -1;
+    while(s <= e){
+        long long int sqr = mid * mid;
+   
+        if(sqr == x){
+         return mid;
+    }
+       else if ( sqr < x){
+           ans  = mid;
+           s = mid + 1;
+       }
+        else{
+            e = mid - 1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+    
+}
+
 class Solution {
-
-public:
-
-    int mySqrt(int x) {
-
-        int low = 0 ;
-
-        int high = x ;
-
-        while(low<=high)
-
-        {
-
-            long long mid = (low+high)/2;
-
-            if(mid*mid==x) return mid;
-
-            else if(mid*mid>x) high = mid-1;
-
-            else  low = mid +1 ;
-
-           
-
-        }
-
-        // return the lower bound of the number
-
-        return high;
-
-        }
-
+public:int mySqrt(int x) {
+             return sqrt(x);
+    
+  }  
 };
