fea78b556b59ebf21d730f06d43860ad427a7f56
Prem Kumar Singh <premkumarsingh355@gmail.com>
2024-04-01 15:31:18 +0000

    1991 - Find the Middle Index in Array - Runtime - 8 ms (0.90%), Memory - 14.7 MB (100.00%)

---
+++ Leetcode-Submissions/1991-find-the-middle-index-in-array/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+               int total = accumulate(nums.begin(),nums.end(),0);
+
+        
+
+       int leftSum = 0;
+
+       
+
+        for(int i = 0; i < nums.size(); i++){
+
+          total -= nums[i];
+
+           if(leftSum ==  total){
+
+               return i;
+
+           }
+
+        leftSum += nums[i];
+
+        }
+
+       return -1;
+    }
+};
