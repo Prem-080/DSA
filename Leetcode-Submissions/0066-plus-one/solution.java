079bf2844867c3e5bdd18d63a84fd5e838815d4b
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-01-18 15:34:49 +0000

    0066 - Plus One - Runtime - 0 ms (100.00%), Memory - 43.6 MB (8.54%)

--- Leetcode-Submissions/0066-plus-one/solution.java
+++ Leetcode-Submissions/0066-plus-one/solution.java
@@ -2,19 +2,20 @@ import java.math.BigInteger;
 
 class Solution {
     public int[] plusOne(int[] digits) {
-        int len = digits.length;
-        for(len = len - 1; len >= 0; len-- ){
-            if(digits[len] != 9){
-                digits[len]++;
-                return digits;
-            }
-            digits[len] = 0; 
+       int len = digits.length;
+       for(int i = len - 1; i >= 0; i--){
+        // Every case:
+        if(digits[i] != 9){
+            digits[i]++;
+            return digits;
         }
-        digits = new int[digits.length + 1];
+        // 119 case
+        digits[i] = 0;
+       }
+       // 999 case.
+        digits = new int[len + 1];
         digits[0] = 1;
         return digits;
-
-        
     }
 
 }
