35f46e7c57e694ce477172a8a2d7caf27deaf79e
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-01-19 08:54:45 +0000

    1281 - Subtract the Product and Sum of Digits of an Integer - Runtime - 0 ms (100.00%), Memory - 7.9 MB (20.86%)

--- Leetcode-Submissions/1281-subtract-the-product-and-sum-of-digits-of-an-integer/solution.cpp
+++ Leetcode-Submissions/1281-subtract-the-product-and-sum-of-digits-of-an-integer/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        int sum = 0, product = 1;
-          for(n;n!=0;n/=10){
-           int a = n % 10;
-           sum = sum + a;
-           product = product * a;
-           }
-         int result = product - sum;
-       return result;
+        int prod = 1, sum = 0;
+        while(n > 0){
+            int rem = n% 10;
+            prod *= rem;
+            sum += rem;
+            n /= 10;
+        }
+        return prod - sum;
     }
 };
