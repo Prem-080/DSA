54708bf2aa0384e1dda7c12f932242a84e164bda
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-02-05 17:44:56 +0000

    1207 - Unique Number of Occurrences - Runtime - 2 ms (97.95%), Memory - 44 MB (7.54%)

--- Leetcode-Submissions/1207-unique-number-of-occurrences/solution.java
+++ Leetcode-Submissions/1207-unique-number-of-occurrences/solution.java
@@ -1,18 +1,36 @@
+import java.util.HashMap;
+import java.util.ArrayList;
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        int [] freq = new int[1001];
-        int [] map = new int[2001];
-        for(int i: arr) {
-            freq[map[1000+i]]--;
-            map[1000+i]++;
-            freq[map[1000+i]]++;
+        HashMap<Integer, Integer> freq = new HashMap<>(); // unique elements (5): frequencies
+        // ArrayList<Integer> seen = new ArrayList<>();
+        // int[] values = new int[freq.size()];
+        for(int i: arr){
+            // if(freq.containsKey(i)){
+            //     freq.put(i, freq.get(i) + 1);
+            //     continue;
+            // }
+            // freq.put(i,1);
+            freq.put(i, freq.getOrDefault(i, 0) + 1); 
+            
         }
 
-        for(int f: freq) {
-            if(f > 1) {
-                return false;
-            }
-        }
-        return true;
+        HashSet<Integer> set = new HashSet<>(freq.values());
+        return freq.size() == set.size();
+
+
+        //check if all are unique.
+        // for(int value: freq.values()){
+        //     if(seen.contains(value)) return false;
+        //     seen.add(value);
+        // }
+
+        // [0, 0] 0-0 = 0
+        // for(int i = 0; i < values.length - 1; i++){
+        //     if(values[i] - values[i + 1] == 0) return false;
+        // }
+
+        // return true;
+
     }
 }
