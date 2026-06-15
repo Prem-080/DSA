788530856c5f236d816bb49aa2b883d48cdb8588
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-06-15 08:53:31 +0000

    1365 - How Many Numbers Are Smaller Than the Current Number - Runtime - 11 ms (5.98%), Memory - 45.9 MB (11.94%)

--- Leetcode-Submissions/1365-how-many-numbers-are-smaller-than-the-current-number/solution.java
+++ Leetcode-Submissions/1365-how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        Map<Integer, Integer> index = new HashMap<>();
-        int[] temp = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(temp);
-        for(int i = 0; i < temp.length; i++){
-            index.put(temp[i], index.getOrDefault(temp[i], i));
-        }
-
+        Map<Integer, Integer> freq = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-           nums[i] = index.get(nums[i]);
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+        for(int i = 0; i < nums.length; i++){
+            int count = 0;
+            int j = 0;
+            while(j < nums[i]){
+                count += freq.getOrDefault(j, 0);
+                j++;
+            }
+            nums[i] = count;
         }
 
         return nums;   
