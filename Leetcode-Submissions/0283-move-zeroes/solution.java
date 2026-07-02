c1e7c63347714b13404cd8251767935405adbd2e
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-07-02 13:19:28 +0000

    0283 - Move Zeroes - Runtime - 2 ms (92.04%), Memory - 47.6 MB (76.86%)

---
+++ Leetcode-Submissions/0283-move-zeroes/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1){
+            return;
+        }
+
+        for(int i = j + 1; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
+        }
+    }
+}
