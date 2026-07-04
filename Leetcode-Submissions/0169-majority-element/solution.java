d7d73377fd8321a0134b1fd96418e327c697fef1
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-07-04 06:57:17 +0000

    0169 - Majority Element - Runtime - 2 ms (70.96%), Memory - 55.7 MB (47.08%)

--- Leetcode-Submissions/0169-majority-element/solution.java
+++ Leetcode-Submissions/0169-majority-element/solution.java
@@ -1,18 +1,28 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int count = 0;
-        int mid = n/2;
-        Map<Integer, Integer> mp = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        int c = 0;
+        int e = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(c == 0){
+                e = nums[i];
+                c++;
+            }
+            else if(e == nums[i]){
+                c++;
+            }
+            else if(e != nums[i]){
+                c--;
+            }
         }
-        Set<Integer> keys = mp.keySet();
-        List<Integer> myList = new ArrayList<>(keys);
-        int i =0;
-        for(int value:  mp.values()){
-            if(value > mid) return myList.get(i);
-            i++;
+
+        int c1 = 0;
+        for(int i : nums){
+            if(i == e){
+                c1++;
+            }
+        }
+        if(c1 > nums.length/2){
+            return e;
         }
         return -1;
     }
