d3cd9d67cd4494a6001f398192e9457037a5cfd6
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-07-07 07:47:41 +0000

    0121 - Best Time to Buy and Sell Stock - Runtime - 1 ms (99.95%), Memory - 94.7 MB (15.79%)

---
+++ Leetcode-Submissions/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(min > prices[i]){
+                min = prices[i];
+            }
+            // calculate the profit.
+            else if(prices[i] - min > profit){
+                profit = prices[i] - min;
+            }      
+        }
+        
+        return profit;
+    }
+}
