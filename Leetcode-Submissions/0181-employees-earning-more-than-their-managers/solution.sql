84c8162faf72e023e50deec5d534e67e9dc3c812
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-07-19 04:12:53 +0000

    0181 - Employees Earning More Than Their Managers - Runtime - 331 ms (99.77%), Memory - 0.0B (100.00%)

--- Leetcode-Submissions/0181-employees-earning-more-than-their-managers/solution.sql
+++ Leetcode-Submissions/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,5 +1,5 @@
-SELECT e2.name as Employee
+SELECT e1.name as Employee
 FROM employee e1
-INNER JOIN employee e2 ON e1.id = e2.managerID
-WHERE e1.salary < e2.salary
+JOIN employee e2 on e1.managerId = e2.id
+where e1.salary > e2.salary;
 
