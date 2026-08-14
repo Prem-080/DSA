081f1c6adcaf37da53f2e3e61a83e7675256ae20
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-14 09:41:03 +0000

    0015 - 3Sum - Runtime - 27 ms (98.84%), Memory - 59.2 MB (45.89%)

--- Leetcode-Submissions/0015-3sum/solution.java
+++ Leetcode-Submissions/0015-3sum/solution.java
@@ -4,33 +4,33 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length-2; i++){
-            if(nums[i] > 0){
+            if(nums[i] > 0){ // if the 1st index is a positive number then adding all pos numbers don't yield to 0.
                 break;
             }
 
-            if(i > 0 && nums[i] == nums[i-1]){
+            if(i > 0 && nums[i] == nums[i-1]){ // skipping duplicate value;
                 continue;
             }
             
             int j = i+1, k = nums.length-1; 
-            while(j < k){
+            while(j < k){ // 2 pointers
                 int sum = nums[i] + nums[j] + nums[k];
                 
-                if (sum < 0){
+                if (sum < 0){ // if sum is smaller then we need to increase the left pointer for higher sum.
                     j++;
                 }
-                else if (sum > 0){
+                else if (sum > 0){ // if sum is greater then we need to decrease the right pointer for lower sum.
                     k--;
                 }
-                else{
+                else{ // if the sum is 0 add it to the result.
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
 
-                    while(j < k){
+                    while(j < k){ // skip duplicate values from left to right
                         j++;
                         if(nums[j] != nums[j-1]) break;
                     }
 
-                    while(j < k){
+                    while(j < k){ // skip duplicates from right to left.
                         k--;
                         if(nums[k] != nums[k+1]) break;
                     }
