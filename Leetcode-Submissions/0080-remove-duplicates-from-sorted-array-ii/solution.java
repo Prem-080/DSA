b13f36ccbb963920e924aae53e453167a2d40dc4
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-15 17:38:02 +0000

    0080 - Remove Duplicates from Sorted Array II - Runtime - 0 ms (100.00%), Memory - 48.7 MB (70.75%)

---
+++ Leetcode-Submissions/0080-remove-duplicates-from-sorted-array-ii/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (i < 2 || nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
