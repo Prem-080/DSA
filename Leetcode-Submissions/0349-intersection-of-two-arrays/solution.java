ec019a9f4bd60b899ba8aa5d0fa1f214196705c0
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-16 10:16:21 +0000

    0349 - Intersection of Two Arrays - Runtime - 3 ms (46.51%), Memory - 44.3 MB (97.77%)

---
+++ Leetcode-Submissions/0349-intersection-of-two-arrays/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> n1 = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums1){
+            n1.add(num);
+        }
+        for(int num: nums2){
+            if(n1.contains(num)){
+                set.add(num);
+            }
+        }
+        int[] res = new int[set.size()];
+        int i = 0;
+        for(int num: set){
+            res[i++] = num;
+        }
+
+        return res;
+
+    }
+}
