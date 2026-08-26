b97758c7e08aeab6b269a4afd752c902e1fb50f0
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-26 12:55:31 +0000

    0367 - Valid Perfect Square - Runtime - 0 ms (100.00%), Memory - 42.4 MB (5.81%)

--- Leetcode-Submissions/0367-valid-perfect-square/solution.java
+++ Leetcode-Submissions/0367-valid-perfect-square/solution.java
@@ -4,10 +4,9 @@ class Solution {
         while(l <= r){
             int mid = l + (r - l) / 2;
             if(mid * mid == num) return true;
-            if(mid > num / mid) r = mid - 1;
-            else l = mid + 1;
+            else if(mid < num / mid) l = mid + 1;
+            else r = mid - 1;
         }
         return false;
-
     }
 }
