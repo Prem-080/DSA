830b23e7ef8ac475e7e083cbb9f54da0eee5d95b
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-28 18:50:40 +0000

    0033 - Search in Rotated Sorted Array - Runtime - 0 ms (100.00%), Memory - 43.7 MB (65.99%)

--- Leetcode-Submissions/0033-search-in-rotated-sorted-array/solution.java
+++ Leetcode-Submissions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,42 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length, ans = -1;
-        int st = 0, en = n - 1;
-        while(st <= en){
-            int mid = st + (en - st) / 2;
-            if(mid < en && nums[mid] > nums[mid + 1]) {
-                st = mid + 1;
-                ans = mid;
-            }
-            else if(mid > st && nums[mid] < nums[mid - 1]){
-                en = mid - 1;
-                ans = mid - 1;
-            } 
-            else if(nums[st] >= nums[mid]) en = mid - 1;
-            else if(nums[st] <= nums[mid]) st = mid + 1;
-
-        }
-
-        if(target > nums[n - 1]){
-            return binarySearch(nums, 0, ans, target);
-        }
-        return binarySearch(nums, ans + 1, n - 1, target);
-    }
-
-    static int binarySearch(int[] arr, int st, int en, int target){
-        while(st <= en){
-            int mid = st + (en - st) / 2;
-            if(arr[mid] < target){
-                st = mid + 1;
-            }
-            else if(arr[mid] > target){
-                en = mid - 1;
-            }
-            else{
+        int n = nums.length;
+        int i = 0, j = n - 1;
+        while(i <= j){
+            int mid = i + (j - i) / 2;
+            if(nums[mid] == target){
                 return mid;
             }
+            else if(nums[mid] < target){ // the target lies ahead the mid or before the mid
+                if(nums[mid] < nums[n - 1] && target > nums[n - 1]){ // if mid is in the left part 
+                    // then move i
+                    j = mid - 1;
+                }
+                else{
+                    i = mid + 1;
+                }
+            }
+            else{// the target either lies before mid or after mid
+                if(nums[mid] > nums[n - 1] && target <= nums[n - 1]){
+                    i = mid + 1;
+                }
+                else{
+                    j = mid - 1;
+                }
+                
+            }
+
         }
         return -1;
+        
     }
 }
-
