91ef264c27eb78c64541dc2d9ebbda7a9bce9a6f
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-08-29 07:44:27 +0000

    0209 - Minimum Size Subarray Sum - Runtime - 2 ms (21.20%), Memory - 69.2 MB (67.15%)

--- Leetcode-Submissions/0209-minimum-size-subarray-sum/solution.java
+++ Leetcode-Submissions/0209-minimum-size-subarray-sum/solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int j = 0;
-        int sum = 0;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            while (sum >= target) {
-                if(min > (i - j + 1)){
+            while(sum >= target){
+                sum -= nums[j];
+                if(min > i - j + 1){
                     min = i - j + 1;
                 }
-                sum -= nums[j++];
+                j++;
             }
         }
         if(min == Integer.MAX_VALUE){
