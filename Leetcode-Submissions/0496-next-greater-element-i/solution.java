feb030c8bde826c186531b9026eaeec5f0d69ce3
Prem Kumar Singh <premkumarsingh355@gmail.com>
2026-09-15 15:44:36 +0000

    0496 - Next Greater Element I - Runtime - 4 ms (73.41%), Memory - 45.1 MB (61.67%)

--- Leetcode-Submissions/0496-next-greater-element-i/solution.java
+++ Leetcode-Submissions/0496-next-greater-element-i/solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        Map<Integer, Integer> mp = new HashMap<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+
         for(int i = nums2.length - 1; i >= 0; i--){
-
-            while(!stack.isEmpty() && stack.peek() <= nums2[i]){
-                stack.pop();
+            // find the greater element in the stack. pop until we get it.
+            while(!s.isEmpty() && nums2[i] >= s.peek()){
+                s.pop();
             }
-            int greater = stack.isEmpty() ? -1 : stack.peek();
-            mp.put(nums2[i], greater);
-            stack.push(nums2[i]);
-        }
-         // Get answers for nums1
-        int[] ans = new int[nums1.length];
+            if(s.isEmpty()){
+                mp.put(nums2[i], -1);
+            }
+            else mp.put(nums2[i], s.peek());
 
-        for (int i = 0; i < nums1.length; i++) {
-            ans[i] = mp.get(nums1[i]);
+            s.push(nums2[i]);
         }
-
-        return ans;
+        int[] res = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++){
+            res[i] = mp.get(nums1[i]);
+        }
+        return res;
     }
 }
